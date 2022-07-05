bfc5ebd270672bb317d45cc4b38d891d16ce90fc
Monica-Lara29 <monicajuliethlara@gmail.com>
2022-07-05 07:53:50 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SocialAuthService, FacebookLoginProvider, SocialUser} from '@abacritt/angularx-social-login';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'LoginFacebook';
+  socialUser!: SocialUser;
+  isLogin!: Boolean; /*variable de control */
+
+  constructor(
+    /*inyección de dependencia*/
+    private socialAuthService: SocialAuthService
+  ){
+  }
+
+  ngOnInit(): void{
+    this.socialAuthService.authState.subscribe((user)=> {
+      /*escucha el estado del login */
+      console.log("Datos del usuario");
+      console.log(user);
+
+      this.socialUser=user;
+      /*evaluación si el objeto trae datos o no */
+      this.isLogin= (user!= null);
+    });
+  }
+  
+  /*funcion que loguea un usuario*/
+  logInWithFacebook():void{
+    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
+   
+  }
+  /*función que cierra sesión */
+  signOut(): void {
+    this.socialAuthService.signOut();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule}  from '@angular/forms';
+import { FacebookLoginProvider, SocialLoginModule, SocialAuthServiceConfig} from '@abacritt/angularx-social-login';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    SocialLoginModule /*modulos que necesitamos importar*/
   ],
-  providers: [],
+  providers: [
+    {
+      /* configuraciones necesarias para obtener el identificador de la app 
+      anteriormente creada */
+      provide: 'SocialAuthServiceConfig',
+      useValue:{
+        autoLogin: false,
+        providers: [{
+          id:FacebookLoginProvider.PROVIDER_ID,
+          provider:new FacebookLoginProvider(
+            '1204062823725121'
+          )
+        }]
+      }as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
